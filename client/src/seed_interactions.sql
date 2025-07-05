024b011c10c7449a707ff3f7d14e4eeb65d9294e
swagranger011 <abhisht01@gmail.com>
2025-07-05 00:12:10 -0500

    Added more interactions

--- client/src/seed_interactions.sql
+++ client/src/seed_interactions.sql
@@ -8,24 +8,24 @@ INSERT INTO DrugInteractions (Drug1ID, Drug2ID, Severity, Description, Mechanism
  'Avoid combination or monitor bleeding time'),
  
 ((SELECT DrugID FROM Drugs WHERE Name = 'Simvastatin'),
- (SELECT DrugID FROM Drugs WHERE Name = 'Ciprofloxacin'),
+ (SELECT DrugID FROM Drugs WHERE Name = 'Cipro'),
  'Moderate',
  'Increased statin levels',
  'CYP3A4 inhibition',
  'Use lower statin dose or alternative antibiotic');
 
  (SELECT DrugID FROM Drugs WHERE Name = 'Metformin'),
- (SELECT DrugID FROM Drugs WHERE Name = 'Ciprofloxacin'),
+ (SELECT DrugID FROM Drugs WHERE Name = 'Cipro'),
  'Moderate',
  'Increased risk of lactic acidosis',
- 'Ciprofloxacin may impair renal function',
+ 'Cipro may impair renal function',
  'Monitor renal function and consider alternative antibiotic');
 
 ((SELECT DrugID FROM Drugs WHERE Name = 'Warfarin'),
-(SELECT DrugID FROM Drugs WHERE Name = 'Ciprofloxacin'),
+(SELECT DrugID FROM Drugs WHERE Name = 'Cipro'),
  'Major',
  'Increased bleeding risk',
- 'Ciprofloxacin may displace warfarin from protein binding sites',
+ 'Cipro may displace warfarin from protein binding sites',
  'Monitor INR closely and adjust warfarin dose as needed'),
 
 ((SELECT DrugID FROM Drugs WHERE Name = 'Warfarin'),
@@ -43,7 +43,7 @@ INSERT INTO DrugInteractions (Drug1ID, Drug2ID, Severity, Description, Mechanism
  'No special management required');
 
 ((SELECT DrugID FROM Drugs WHERE Name = 'Aspirin'),
- (SELECT DrugID FROM Drugs WHERE Name = 'Ciprofloxacin'),
+ (SELECT DrugID FROM Drugs WHERE Name = 'Cipro'),
  'Moderate',
  'Increased risk of gastrointestinal bleeding',
  'Both drugs can cause GI irritation',
@@ -54,6 +54,61 @@ INSERT INTO DrugInteractions (Drug1ID, Drug2ID, Severity, Description, Mechanism
  'Low',
  'No significant interaction',
  NULL,
- 'No special management required');
+ 'No special management required'),
 
+((SELECT DrugID FROM Drugs WHERE Name = 'Prozac'),
+(SELECT DrugID FROM Drugs WHERE Name = 'Abilify'),
+ 'Moderate',
+ 'Increased risk of serotonin syndrome',
+ 'Both drugs increase serotonin levels',
+ 'Monitor for symptoms of serotonin syndrome and consider dose adjustments'),
+
+((SELECT DrugID FROM Drugs WHERE Name = 'Prozac'),
+ (SELECT DrugID FROM Drugs WHERE Name = 'Cipro'),
+ 'Low',
+ 'No significant interaction',
+ NULL,
+ 'No special management required'),
+
+((SELECT DrugID FROM Drugs WHERE Name = 'Prozac'),
+ (SELECT DrugID FROM Drugs WHERE Name = 'Metformin'),
+ 'Low',
+ 'No significant interaction',
+ NULL,
+ 'No special management required'),
+
+((SELECT DrugID FROM Drugs WHERE Name = 'Prozac'),
+ (SELECT DrugID FROM Drugs WHERE Name = 'Simvastatin'),
+ 'Low',
+ 'No significant interaction',
+ NULL,
+ 'No special management required'),
+
+((SELECT DrugID FROM Drugs WHERE Name = 'Zoloft'),
+ (SELECT DrugID FROM Drugs WHERE Name = 'Abilify'),
+ 'Moderate',
+ 'Increased risk of serotonin syndrome',
+ 'Both drugs increase serotonin levels',
+ 'Monitor for symptoms of serotonin syndrome and consider dose adjustments'),
+
+((SELECT DrugID FROM Drugs WHERE Name = 'Zoloft'),
+ (SELECT DrugID FROM Drugs WHERE Name = 'Cipro'),
+ 'Low',
+ 'No significant interaction',
+ NULL,
+ 'No special management required'),
+
+((SELECT DrugID FROM Drugs WHERE Name = 'Zoloft'),
+ (SELECT DrugID FROM Drugs WHERE Name = 'Metformin'),
+ 'Low',
+ 'No significant interaction',
+ NULL,
+ 'No special management required'),
+
+((SELECT DrugID FROM Drugs WHERE Name = 'Zoloft'),
+ (SELECT DrugID FROM Drugs WHERE Name = 'Simvastatin'),
+ 'Low',
+ 'No significant interaction',
+ NULL,
+ 'No special management required');
  
